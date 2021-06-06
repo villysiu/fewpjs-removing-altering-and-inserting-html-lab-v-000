f14a4d6aa4b11405c6b8f4ff41b28667fae31136
villysiu <villysiu@gmail.com>
2021-06-06 13:58:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1")
+
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "YOUR-NAME is the champion"
